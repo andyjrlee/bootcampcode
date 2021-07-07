37d57fbc9eb7d589efea327a89ebb2121a963a22
andyjrlee <andyjrlee@outlook.com>
2021-07-07 10:24:40 +0100

    uploading results code

--- src/results.java
+++ src/results.java
@@ -7,7 +7,7 @@ public class results {
 	System.out.println("Chemistry Results - " + Chemistry(150));
 	System.out.println("Biology Results - " + Biology(150));
 	System.out.println("Total Exam Score - " + TotalExam(150,150,150));
-	System.out.println("Exam Percentage - " + examPercentage(150, 150, 150));
+	System.out.println("Exam Percentage - " + examPercentage(150, 150, 150) + " %");
 	
 		//int Physics;
 		//int Chemistry;
@@ -17,17 +17,14 @@ public class results {
 	}
 	
 	public static int Physics(int number1) {
-		int ans = number1;
-		return ans;
+		return number1;
 	}
 	public static int Chemistry(int number1) {
-		int ans = number1;
-		return ans;
+		return number1;
 	}
 	
 	public static int Biology(int number1) {
-		int ans = number1;
-		return ans;
+		return number1;
 	}
 	
 	public static int TotalExam(int number1, int number2, int number3) {
